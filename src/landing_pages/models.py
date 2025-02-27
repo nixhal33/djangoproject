8c9d9a330f052b9b45aa477412370a10c941c164
nixhal33 <nischal.koirala33@gmail.com>
2025-02-27 22:45:54 +0545

    updated

--- src/landing_pages/models.py
+++ src/landing_pages/models.py
@@ -14,6 +14,7 @@ so that your database and model alway synced up..!! :)
 class LandingPageEntry(models.Model):
     name=models.CharField(max_length=85, blank=False, null=False)
     email=models.EmailField()
+    password=models.CharField(max_length=128)
 
     def __str__(self):
         return f"{self.name}" # i can change this self.name into self.email where i can see changes in the admin panel where it will show email of those LandingpageEntry as name of entrants
--- src/landing_pages/views.py
+++ src/landing_pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . forms import LandingPageForm
-
+from .models import LandingPageEntry
 
 def home_page(request, *args, **kwargs):
     title = "Welcome Home"
@@ -8,6 +8,14 @@ def home_page(request, *args, **kwargs):
     if form.is_valid():
         # print(form.cleaned_data.get("email"))
         print(form.cleaned_data)
+        name = form.cleaned_data.get("name")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        obj = LandingPageEntry.objects.create(
+            name=name,
+            email=email,
+            password=password
+        )
         form=LandingPageForm(None)
     # print("Your email is :", request.POST.get("email"))
     context = { 
